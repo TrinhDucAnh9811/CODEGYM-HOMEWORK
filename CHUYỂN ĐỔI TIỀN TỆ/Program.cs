90b438ebb3a66cc889bf6a48eacf783f631ae4e7
Your Name <you@example.com>
2023-08-18 09:38:27 +0700

    CHUYỂN ĐỔI TIỀN TỆ

---
+++ CHUYỂN ĐỔI TIỀN TỆ/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace DucANh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double exchangeRate = 23830;
+
+            Console.WriteLine("Enter the amount of USD: ");
+            double uSD = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Value when convert from USD to VND is: " + uSD*exchangeRate + " dong. ");
+        }
+    }
+}
