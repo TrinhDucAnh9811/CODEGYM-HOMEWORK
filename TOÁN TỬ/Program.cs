3343bfcb9d4edf4e39102b099e9b88295e893c95
Your Name <you@example.com>
2023-08-18 09:25:54 +0700

    TOÁN TỬ

---
+++ TOÁN TỬ/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ducAnh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter the Width: ");
+            float width = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please Enter the Height: ");
+            float height = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Area of the rectangle is: " + height*width);
+        }
+}
+}
