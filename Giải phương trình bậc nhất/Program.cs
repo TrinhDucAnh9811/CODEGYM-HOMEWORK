72e08a0dcfde1258f7cd08e1c1b7e6cfcd4fd43f
Your Name <you@example.com>
2023-08-18 14:08:59 +0700

    Giải phương trình bậc nhất

---
+++ Giải phương trình bậc nhất/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace DucAnh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linear Equation Resolver");
+            Console.WriteLine("Given a equation as 'ax + b = 0', please enter constants: ");
+
+            double result;
+
+            Console.WriteLine("Enter number a: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter number b: ");
+            double b = double.Parse(Console.ReadLine());
+
+            if(a==0)
+            {
+                if(b==0)
+                {Console.WriteLine("Infinite Solutions");}
+
+                if(b!=0)
+                {Console.WriteLine("No Solution");}
+            }
+            else
+            {
+                result = -b/a;
+                Console.WriteLine("The solution is: " + result);
+            }
+
+        }
+    }
+}
