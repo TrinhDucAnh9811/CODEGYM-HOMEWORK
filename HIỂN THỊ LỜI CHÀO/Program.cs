3f8ff3b254024c34ac26f90405c064155907be34
Your Name <you@example.com>
2023-08-18 09:29:51 +0700

    HIỂN THỊ LỜI CHÀO

---
+++ HIỂN THỊ LỜI CHÀO/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace DucAnh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
