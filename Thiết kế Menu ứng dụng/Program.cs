1407c67dd90075b8e688418cb29bdd5925b5f40b
Your Name <you@example.com>
2023-08-24 14:05:10 +0700

    Thiết kế Menu ứng dụng

--- Thiết kế Menu ứng dụng/Program.cs
+++ Thiết kế Menu ứng dụng/Program.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using System.Globalization;
 
 int choice;
 bool drawAgain = true;
 string response;
+
+int height = 10;
+int width = 10;
+int width_rectangle = 15;
+int width_temp = 10;
+int width_reverse_triangle = 1;
 System.Console.WriteLine("This's a Program for creating your own shape, please see the menu below:");
 System.Console.WriteLine("Menu");
-System.Console.WriteLine("1. Draw Triangle");
-System.Console.WriteLine("2. Draw the Square");
-System.Console.WriteLine("3. Draw the Rectangle");
+System.Console.WriteLine("1. Draw Square");
+System.Console.WriteLine("2. Draw the Rectangle");
+System.Console.WriteLine("3. Draw the Triangle 1");
+System.Console.WriteLine("4. Draw the Triangle 2");
 
 
 while(drawAgain)
@@ -17,29 +25,51 @@ while(drawAgain)
     switch(choice)
     {
         case 1:
-        Console.WriteLine("******");
-        Console.WriteLine("*****");
-        Console.WriteLine("****");
-        Console.WriteLine("***");
-        Console.WriteLine("**");
-        Console.WriteLine("*");
+        for(int i = 0; i<height; i++)
+        {
+            for(int j=0; j<width;j++ )
+            {
+                Console.Write("* ");
+            }
+            System.Console.WriteLine("");
+        }
         break;
 
         case 2:
-        Console.WriteLine("Draw the square");
-        Console.WriteLine("* * * * * *");
-        Console.WriteLine("* * * * * *");
-        Console.WriteLine("* * * * * *");
-        Console.WriteLine("* * * * * *");
-        Console.WriteLine("* * * * * *");
-        Console.WriteLine("* * * * * *");
+        for(int i = 0; i<height; i++)
+        {
+            for(int j=0; j<width_rectangle;j++ )
+            {
+                Console.Write("* ");
+            }
+             System.Console.WriteLine("");
+        }
         break;
 
         case 3:
-        Console.WriteLine("* * * * * *");
-        Console.WriteLine("* * * * * *");
-        Console.WriteLine("* * * * * *");
+        for(int i = 0; i<height; i++)
+        {
+            for(int j=0; j<width_temp;j++ )
+            {
+                Console.Write("* ");
+            }
+            width_temp --;
+            System.Console.WriteLine("");
+        }
         break;
+
+        case 4:
+        for(int i = 0; i<height; i++)
+        {
+            for(int j=0; j<width_reverse_triangle;j++ )
+            {
+                Console.Write("* ");
+            }
+            width_reverse_triangle ++;
+            System.Console.WriteLine("");
+        }
+        break;
+
     }
 
     System.Console.WriteLine("Do you want to draw again? (Y/N)");
