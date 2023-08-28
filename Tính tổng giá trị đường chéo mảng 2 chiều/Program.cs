11d9c18c8261cd12ca5ad3ad37597872fab99737
Your Name <you@example.com>
2023-08-28 16:52:08 +0700

    Tổng giá trị đường chéo chính mảng 2 chiều

---
+++ Tính tổng giá trị đường chéo mảng 2 chiều/Program.cs
@@ -0,0 +1,26 @@
+﻿﻿using System;
+
+namespace FindDiagonalValue
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            int[,] mang_hai_chieu = {
+                { 3, 8, 2 },
+                { 10, 5, 6 },
+                { 1, 7, 9 }
+            };
+            
+            int total = 0;
+            
+            for (int i = 0; i < mang_hai_chieu.GetLength(0); i++)
+            {total += mang_hai_chieu[i, i];}
+
+            Console.WriteLine("Total value of diagonal in 2D Array: " + total);
+        }
+            
+    }
+}
