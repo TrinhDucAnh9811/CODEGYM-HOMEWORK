d6083e45688afede38e4a5df5735d01c16e26dd2
Your Name <you@example.com>
2023-08-28 16:41:19 +0700

    Tìm giá trị lớn nhất của mảng 2 chiều

---
+++ Tìm giá trị lớn nhất trong mảng 2 chiều/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MaxValue2DArray
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            int[,] mang_hai_chieu = {
+                { 3, 8, 2 },
+                { 10, 4, 6 },
+                { 1, 7, 9 }
+            };
+            
+            int maxValue = int.MinValue;
+
+            for (int i = 0; i < mang_hai_chieu.GetLength(0); i++)
+            {
+                for (int j = 0; j < mang_hai_chieu.GetLength(1); j++)
+                {
+                    if (mang_hai_chieu[i, j] > maxValue)
+                    {
+                        maxValue = mang_hai_chieu[i, j];
+                    }
+                }
+            }
+            Console.WriteLine("Max value in the 2D array: " + maxValue);
+        }
+    }
+}
