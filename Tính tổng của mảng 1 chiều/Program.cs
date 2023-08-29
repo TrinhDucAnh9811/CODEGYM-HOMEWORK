801e2a59e1fffcd72acb2faf9915801330a08502
Your Name <you@example.com>
2023-08-29 11:13:22 +0700

    Tính tổng mảng 1 chiều, BTVN CodeGym

---
+++ Tính tổng của mảng 1 chiều/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Minesweeper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mang = { 1, 2, 4, 5, 31 };
+            int total = 0;
+            for (int i = 0; i < mang.Length; i++)
+            {
+                total += mang[i];
+            }
+            Console.WriteLine($"Tong cua mang 1 chieu: {total}");
+            Console.ReadKey();
+        }
+    }
+}
