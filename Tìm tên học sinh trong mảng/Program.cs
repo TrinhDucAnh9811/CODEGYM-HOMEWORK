10f8142c7a511ae62f0db8a27beb63f3fb546300
Your Name <you@example.com>
2023-08-29 12:39:11 +0700

    Tìm tên học sinh trong mảng

---
+++ Tìm tên học sinh trong mảng/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DucAnh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isTrue = false;
+            string[] students = { "Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily" };
+            Console.WriteLine("Enter a name’s student:");
+            string input_name = Console.ReadLine();
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                if (input_name == students[i])
+                {
+                    isTrue = true;
+                    Console.WriteLine($"The position of student named {input_name} is: {i + 1}");
+                    break;
+                }
+                
+            }
+            if (!isTrue)
+            {
+                Console.WriteLine("There's no student that named " + input_name + " in the array");
+            }
+            Console.ReadKey();
+        }
+    }
+}
