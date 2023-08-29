6452dd0372ceb88145772f5f35a5f99862228641
Your Name <you@example.com>
2023-08-29 13:41:21 +0700

    Chèn số vào mảng

---
+++ Chèn số vào mảng và lùi thứ tự (Nên đọc lại)/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+
+namespace MaxValue2DArray
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            int[] mang = { 1, 2, 3, 4, 5, 6 };
+            Console.WriteLine("Nhap so can chen vao mang: ");
+            int so_can_chen = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nhap vi tri can chen vao mang: ");
+            int vi_tri_chen = int.Parse(Console.ReadLine());
+
+            if(vi_tri_chen >mang.Length-1 || vi_tri_chen<0 )
+            { Console.WriteLine("Khong the chen vao trong mang"); }
+
+            else
+            {
+                for (int i = mang.Length - 1; i > vi_tri_chen; i--)
+                {
+                    mang[i] = mang[i - 1];
+                }
+                mang[vi_tri_chen] = so_can_chen;
+
+            }
+
+            IN_MANG(mang);
+            Console.ReadKey();
+        }
+
+
+        public static void IN_MANG(int[] mang)
+        {
+            for(int i = 0; i < mang.Length; i++)
+            {
+                Console.Write(mang[i] + " ");
+            }
+        }
+    }
+}
