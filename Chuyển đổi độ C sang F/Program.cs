03ec1bc68af822362f31fa7b96756ea2944871a3
Your Name <you@example.com>
2023-08-30 11:10:04 +0700

    Update

--- Chuyển đổi độ C sang F/Program.cs
+++ Chuyển đổi độ C sang F/Program.cs
@@ -4,19 +4,64 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("This's a program to convert Celsius to Fahrenheit and in versa");
-        Console.WriteLine("--------------------------------------------------------------");
+        double celsius;
+        double Fehrenheit;
+        int choice;
+        string option;
+        bool playAgain = true;
 
-        //Convert C to F:
-        Console.WriteLine("Please enter the Celsius degree: ");
-        double celsius = double.Parse(Console.ReadLine());
-        Console.WriteLine($"The Fehrenheit is: {celsiusToFehrenheit(celsius)}\n");
+        
 
+        while (playAgain)
+        {
+            Console.WriteLine("This's a program to convert Celsius to Fahrenheit and in versa");
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine("Menu");
+            Console.WriteLine("Please choose one of these options:");
+            Console.WriteLine("1. Fahrenheit to Celsius");
+            Console.WriteLine("2. Celsius to Fahrenheit");
+            Console.WriteLine("3. Exit");
+            choice = int.Parse(Console.ReadLine());
 
-        //Convert F to C:
-        Console.WriteLine("Please enter the Fehrenheit degree: ");
-        double Fehrenheit = double.Parse(Console.ReadLine());
-        Console.WriteLine($"The Celsius is: {FehrenheitToCelsius(Fehrenheit)}\n");
+            switch (choice)
+            {
+                case 1:
+                    {
+                        //Convert F to C:
+                        Console.WriteLine("Please enter the Fehrenheit degree: ");
+                        Fehrenheit = double.Parse(Console.ReadLine());
+                        Console.WriteLine($"The Celsius is: {FehrenheitToCelsius(Fehrenheit)}\n");
+                    }
+                    break;
+
+                case 2:
+                    {
+                        //Convert C to F:
+                        Console.WriteLine("Please enter the Celsius degree: ");
+                        celsius = double.Parse(Console.ReadLine());
+                        Console.WriteLine($"The Fehrenheit is: {celsiusToFehrenheit(celsius)}\n");                   
+                    }
+                    break;
+
+                case 3:
+                    {
+                        playAgain = false;                       
+                    }
+                    break;
+
+            }
+            Console.WriteLine("Do you want to reset and choose again ? Y / N");
+            option = Console.ReadLine().ToUpper();
+            if (option == "Y")
+            {
+                playAgain = true;
+            }
+            else if (option == "N")
+            {
+                playAgain = false;
+            }
+        }
+
 
     }
 
