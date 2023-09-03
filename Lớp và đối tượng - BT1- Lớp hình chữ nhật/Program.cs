8fb10f23ff9ea60026c0b5091c1be7851749de45
Your Name <you@example.com>
2023-09-03 12:53:01 +0700

    BT1: Lớp hình chữ nhật

---
+++ Lớp và đối tượng - BT1- Lớp hình chữ nhật/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace DucAnh
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your width: ");
+            double width = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter you height: ");
+            double height = double.Parse(Console.ReadLine());
+
+            Regtangle regtangle = new Regtangle(width, height);
+
+            Console.WriteLine("The area of the regtangel is:" + regtangle.GetArea());
+            Console.WriteLine("The Perimeter of the regtangle is: " + regtangle.GetPerimeter());
+            Console.WriteLine("The Regtangle \n: " + regtangle.Display());
+        }
+    }
+    class Regtangle
+    {
+        double width, height;
+        public Regtangle() { }
+
+        public Regtangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double GetArea()
+        {
+            return (width * height);
+        }
+
+        public double GetPerimeter()
+        {
+            return (width + height) * 2;
+        }
+
+        public string Display()
+        {
+            return "Rectangle{" + "width=" + width + ", height=" + height + "}";
+        }
+    }
+
+
+
+}
